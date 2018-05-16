70a84cb4178a752897ad583a810eb0b3e9f83a6a
Matt Derynck <derynckmatthew@gmail.com>
2018-05-16 00:17:22 -0400

    Implemented the BankAccount class details, added functionality to the BankDriver to display dialogs for the user to input BankAccount details as well as deposit/withdraw from the account.

--- src/exercise2/BankAccount.java
+++ src/exercise2/BankAccount.java
@@ -6,35 +6,39 @@ public class BankAccount {
     private String ownerName;
     private double balance;
 
+    //returns the account number.
     public int getAccountNumber() {
         return accountNumber;
     }
-
+    //returns the owner of this account.
     public String getOwnerName() {
         return ownerName;
     }
-
+    //returns the balance of this account.
     public double getBalance() {
         return balance;
     }
 
-
+    //BankAccount constructor
     public BankAccount(int accountNumber, String ownerName, double balance) {
         this.accountNumber = accountNumber;
         this.ownerName = ownerName;
         this.balance = balance;
     }
 
+    //Withdraw money from account. *Note -  No overdraft protection.
     public void withdraw(double amount) {
-
+        balance -= amount;
     }
 
+    //Deposit money into account. *Note - I wish it was this easy to get free money.
     public void deposit(double amount) {
-
+        balance += amount;
     }
 
+    //returns all account info in a formatted string.
     public String getAccountInfo(){
-        return "";
+        return String.format("Account Number: %s%nAccount Owner: %s%nAccount Balance: $%.2f%n",getAccountNumber(),getOwnerName(),getBalance());
     }
 
 }
---
+++ src/exercise2/BankDriver.java
@@ -0,0 +1,68 @@
+package exercise2;
+
+import javax.swing.JOptionPane;
+
+public class BankDriver {
+    public static void main(String[] args) {
+
+        //Input variables
+        int accountNumber = 0;
+        String ownerName;
+        double balance;
+
+        //Try to get a valid account number
+        boolean validAccNum = false;
+        do {
+            try {
+                accountNumber = Integer.parseInt(JOptionPane.showInputDialog("Enter your account number"));
+                validAccNum = true;
+            } catch (Exception e) {
+                //Account number was invalid, display dialog and try again.
+                JOptionPane.showMessageDialog(null, "Invalid Account Number!");
+            }
+        }
+        while (!validAccNum);
+
+        //Get account owner name
+        ownerName = JOptionPane.showInputDialog("Enter account owner's name");
+
+        //Try to get a valid balance
+        balance = getInputDouble("Enter account balance","Invalid Balance!");
+
+        //Intialize new bank account with input values.
+        BankAccount newAccount = new BankAccount(accountNumber, ownerName, balance);
+        //Display the account menu.
+        displayAccountMenu(newAccount);
+
+    }
+
+    //Creates a option dialog showing the account information and menu options to deposit, withdraw or exit.
+    public static void displayAccountMenu(BankAccount account) {
+        Object[] options = {"Deposit", "Withdraw", "Exit"};
+        int selection = JOptionPane.showOptionDialog(null, account.getAccountInfo(),
+                "Account Menu", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[2]);
+        //Deposit option selected
+        if (selection == 0) {
+            account.deposit(getInputDouble("Enter amount to deposit into account", "Invalid deposit amount!"));
+            displayAccountMenu(account);
+        } else if (selection == 1) {//Withdraw option selected
+            account.withdraw(getInputDouble("Enter amount to withdraw from account", "Invalid withdrawal amount!"));
+            displayAccountMenu(account);
+        }
+    }
+
+    //Helper method to parse and return a double from user input
+    public static Double getInputDouble(String inputMessage, String exceptionMessage) {
+        double inputDouble;
+        do {
+            try {
+                inputDouble = Double.parseDouble(JOptionPane.showInputDialog(inputMessage));
+                return inputDouble;
+            } catch (Exception e) {
+                //Input was invalid, display exception.
+                JOptionPane.showMessageDialog(null, exceptionMessage);
+            }
+        }
+        while (true);
+    }
+}
